380063532d8e582498f48689564863c925c23c9a
Nitish-aggarwal04 <aggarwalnitish604@gmail.com>
2025-09-07 18:28:26 +0530

    Update 33.py

---
+++ 33.py
@@ -0,0 +1,6 @@
+n = 5
+for i in range(1, n+1):
+    print(" " * (n - i) + "* " * i)
+for i in range(n-1, 0, -1):
+    print(" " * (n - i) + "* " * i)
+print("Program is executed by Nitish 0231BCA017")
