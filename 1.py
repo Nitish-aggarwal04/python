9eac79e4b47882f816a9cc21c0d9bd344458533e
Nitish-aggarwal04 <aggarwalnitish604@gmail.com>
2025-09-07 17:53:42 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,2 @@
+print("Hello World")
+print("Program is executed by Nitish 0231BCA017")
---
+++ 3.py
@@ -0,0 +1,23 @@
+#if-else conditional statement
+variable = 7
+
+if variable > 5:
+    print("variable is greater than 5")
+else:
+    print("variable is 5 or less")
+
+#while loop
+count = 0
+
+while count < 5:
+    print(count)
+    count += 1
+
+#for loop
+for i in range(5):
+    print(i)
+
+#function definition
+def my_function():
+    print("Hello World")
+print("Program is executed by Nitish 0231BCA017")
