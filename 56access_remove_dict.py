3313f6ff23aa87f975f7c6346dae093ef6676528
Nitish-aggarwal04 <aggarwalnitish604@gmail.com>
2025-09-07 18:45:59 +0530

    Update and rename 56.py to 56access_remove_dict.py

---
+++ 56access_remove_dict.py
@@ -0,0 +1,6 @@
+my_dict = {'name': 'Bob', 'age': 30, 'job': 'Engineer'}
+name = my_dict.get('name')
+my_dict.pop('job')
+print(name) 
+print(my_dict) 
+print("Program is executed by Nitish 0231BCA017")
